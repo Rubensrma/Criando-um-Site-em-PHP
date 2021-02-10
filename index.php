b1642b54e6385ca0854abfdf2f1e7636c024dc60
unknown <rubensalves.82@gmail.com>
2021-02-10 16:52:27 -0300

    cria commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+	    <title>Meu primeiro site em PHP! Woohoo!</title>
+	</head>
+	
+    <body>
+
+         <?php
+            for ( $i = 0 ; $i < 10 ; $i++ ) {
+                print ( "Linha número " . $i . "<br />");
+			}
+        ?>
+   	</body>
+
+</html>	
+ 				
